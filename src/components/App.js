0010c44100aa0c5b65fca595c3230edc6302448c
Uttu316 <34169292+Uttu316@users.noreply.github.com>
2024-11-28 22:57:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,37 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
-import './App.css';
+import "./markdown.css";
 
-function App(){
+function Markdown() {
+  const [markdown, setMarkdown] = useState("");
+  const [loading, setLoading] = useState(true);
 
-  const [markdown,setMarkdown] = useState("")
-  const [loading,setLoading] = useState(true)
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
 
-  const handleChange = (e) =>{
-    setMarkdown(e.target.value)
-  }
-  
   const changeLoadingVar = () => {
-    setLoading(false)
-  }
+    setLoading(false);
+  };
 
   useEffect(() => {
-    setTimeout(changeLoadingVar,3000)
-  },[]);
+    setTimeout(changeLoadingVar, 3000);
+  }, []);
 
-    if(loading){
-      return (<div><h1>Loading...</h1></div>)
-    }
- 
-    return(
-      <div className="app">
-        <textarea onChange={handleChange}/>
-        <ReactMarkdown className="preview" children={markdown}/>
+  if (loading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
       </div>
-    )
+    );
+  }
+
+  return (
+    <div className="app">
+      <textarea className="textarea" onChange={handleChange} />
+      <ReactMarkdown className="preview">{markdown}</ReactMarkdown>
+    </div>
+  );
 }
+
+export default Markdown;
